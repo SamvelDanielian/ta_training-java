2f6ba8c98f8af3e35f47f493e627c3128b9507b1
SamvelDanielian <101581730+SamvelDanielian@users.noreply.github.com>
2023-04-16 23:30:25 +0400

    Practical Task "Electronic Watch"

---
+++ electronic-watch/src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -0,0 +1,15 @@
+package com.epam.rd.autotasks.meetautocode;
+
+import java.util.Scanner;
+
+public class ElectronicWatch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int seconds = scanner.nextInt();
+        int num = 10;
+        while  (++num > 20) {
+            num++;
+        }
+        System.out.println(num);
+    }
+}
