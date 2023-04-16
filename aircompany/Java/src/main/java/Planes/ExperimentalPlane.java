5cadf980ccb40c0dfd49108aee47a954a5185312
SamvelDanielian <101581730+SamvelDanielian@users.noreply.github.com>
2023-04-17 01:54:00 +0400

    Practical Task "AirCompany"

---
+++ aircompany/Java/src/main/java/Planes/ExperimentalPlane.java
@@ -0,0 +1,54 @@
+package Planes;
+
+import models.ClassificationLevel;
+import models.ExperimentalTypes;
+
+public class ExperimentalPlane extends Plane{
+
+    private ExperimentalTypes type;
+    private ClassificationLevel classificationLevel;
+
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.type = type;
+        this.classificationLevel = classificationLevel;
+    }
+
+    public ClassificationLevel getClassificationLevel(){
+        return classificationLevel;
+    }
+
+    public void setClassificationLevel(ClassificationLevel classificationLevel){
+        this.classificationLevel = classificationLevel;
+    }
+
+    public ExperimentalTypes getType() {
+        return type;
+    }
+
+    public void setType(ExperimentalTypes type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MilitaryPlane)) return false;
+        if (!super.equals(o)) return false;
+        ExperimentalPlane experimentalPlane = (ExperimentalPlane) o;
+        return type == experimentalPlane.type && classificationLevel == experimentalPlane.classificationLevel;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), type, classificationLevel);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replace("}", ", experimentalPlane{" +
+                "type=" + type +
+                ", classificationLevel=" + classificationLevel +
+                '}');
+    }
+}
