85a4006fe9353d9f1584be5c970a1a230fca6c5f
SamvelDanielian <101581730+SamvelDanielian@users.noreply.github.com>
2023-04-16 23:08:28 +0400

    Practical Task "Average"

---
+++ average/src/main/java/com/epam/rd/autotasks/Average.java
@@ -0,0 +1,25 @@
+package com.epam.rd.autotasks;
+
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int currentValue;
+        int average;
+        int sum = 0;
+        int k = 0;
+
+        do{
+            currentValue = scanner.nextInt();
+            if (currentValue==0){
+                break;
+            }
+            sum += currentValue;
+            k++;
+        }while (currentValue!=0);
+
+        average = sum/k;
+        System.out.println(average);
+    }
+}
